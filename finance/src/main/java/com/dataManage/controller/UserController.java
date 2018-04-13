1c6112181f23511fee0affed875773aa491c8cb6
hy928302776 <928302776@qq.com>
2018-04-13 10:27:20 +0800

    f日志

--- finance/src/main/java/com/dataManage/controller/UserController.java
+++ finance/src/main/java/com/dataManage/controller/UserController.java
@@ -1,18 +1,25 @@
 package com.dataManage.controller;
 
 import com.dataManage.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
+
+
 @Controller
 public class UserController {
     @Autowired
     private UserService userService;
+
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
     @RequestMapping("/")
     public String index(){
+        log.info("index page");
         return "index";
     }
 
